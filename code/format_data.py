f75462461cf41b5effa406429e852f4579e645d6
Stefano Carpita <carpitastefano@gmail.com>
2017-11-12 19:52:05 +0100

    Added data folder, with formatted data

---
+++ code/format_data.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+
+
+df = pd.read_csv('../HR_comma_sep.csv',
+                 names=['Satisfaction_Level', 'Last_Evaluation',
+                        'Number_Project', 'Average_Montly_Hours',
+                        'Time_Spend_Company', 'Work_Accident', 'Left',
+                        'Promotion_Last_5_Years', 'Sales', 'Salary'],
+                 header=0)
+
+
+df_shape=df.shape
+
+## choose an ordering!
+df_formatted = df[['Satisfaction_Level',
+                   'Last_Evaluation',
+                   'Number_Project',
+                   'Average_Montly_Hours',
+                   'Time_Spend_Company',
+                   'Work_Accident',
+                   'Promotion_Last_5_Years',
+                   'Sales',
+                   'Salary',
+                   'Left'
+]]
+
+
+## check and write
+df_formatted_shape=df_formatted.shape
+
+if df_shape==df_formatted_shape:
+    df_formatted.to_csv('../data/df_formatted.csv',index=False)
+
+
+# test
+#df_test = pd.read_csv('../data/df_formatted.csv',header=0)
+#df_test.head()
+
