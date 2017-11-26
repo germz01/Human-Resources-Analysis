2b7fa06a22b97996b3b186829ad28d6eca287557
Stefano Carpita <carpitastefano@gmail.com>
2017-11-26 20:43:58 +0100

    added  silhouette plots

--- code/hierarchical.py
+++ code/hierarchical.py
@@ -88,14 +88,6 @@ out = [data_link,[method,distance,coph_corr,]]
 # testing function finished
 ###########################################################
 
-prova = np.array(cut_tree(data_link,n_clusters=3))
-plt.hist(prova)
-
-plt.close()
-np.corrcoef(np.array(df.Left),prova)
-
-jaccard(df.Left,prova)
-
 
 ###########################################################
 # function for hierarchical clustering
@@ -109,11 +101,6 @@ def analyze(method,distance):
 
     coph = cophenet(Z = data_link, Y = data_dist)
     coph_corr=coph[0]
-
-    labels = fcluster(data_link, 3, criterion="maxclust")
-    silhouette_coefficient = silhouette_score(squareform(data_dist),labels, metric='euclidean')
-
-
     
     plt.close()
 
@@ -127,34 +114,25 @@ def analyze(method,distance):
 
     plt.savefig('../images/hierarchical/dendrogram_{}_{}.pdf'.format(method,distance))
      
-    return [method,distance,coph_corr,silhouette_coefficient]
+    return [method,distance,coph_corr]
 
 ###########################################################
 
 ## apply each method for each metric
+
+methods = ['single','complete','average','weighted','centroid','median','ward']
 
 out = []        
 for method in methods:
     for distance in distances:
         out.append(analyze(method,distance=distance))
 
+###########################################################        
+# which is the better method?
+# Let's compare the cophenetic correlation
 
-methods = ['single','complete','average','weighted','centroid','median','ward']
-
-selected_methods = ['average','centroid']
-
-out = []        
-for method in selected_methods:
-    for distance in distances:
-        out.append(analyze(method,distance=distance))
-        
-        
-import gc
-gc.collect()
-
-    
 Dout =  pd.DataFrame(out)
-Dout.columns = ['method','distance','coph_corr','silhouette']
+Dout.columns = ['method','distance','coph_corr']
 
 Dout = Dout.sort_values(by=['coph_corr','method','distance'],ascending=False)
 
@@ -173,27 +151,120 @@ plt.savefig('../images/hierarchical/methods_comparison.pdf')
 plt.close()
 
 ###########################################################
-# silhouette comparison
+# from the selected method which are the best?
+# where to cut the dendrogram?
+# i.e which is the optimal number of clusters?
+# -> silhouette comparison
+import matplotlib.cm as cm
 
-plt.figure(figsize=(7,5))
-sb.barplot(data=Dout, x='method',y='silhouette',
-           hue='distance')
-plt.title('Silhouette comparison for selected methods,two clusters')
-plt.tight_layout()
+selected_methods = ['average','centroid']
 
-plt.savefig('../images/hierarchical/silhouette_comparison.pdf')
+out = []        
+for method in selected_methods:
+    for distance in distances:
+
+method = 'centroid'        
+distance = 'euclidean'        
+
+data_dist = pdist(data,metric = distance)
+data_link = linkage(data_dist,method=method,metric=distance)
+
+#coph = cophenet(Z = data_link, Y = data_dist)
+#coph_corr=coph[0]
+
+n_clusters = 3
+
+def calc_silhouette(n_clusters):
+    
+    cluster_labels = fcluster(data_link, (n_clusters+1), criterion="maxclust")
+    silhouette_average = silhouette_score(squareform(data_dist),cluster_labels, metric='euclidean')
+    silhouette_values = silhouette_samples(squareform(data_dist),cluster_labels)
+
+    plt.close()
+
+    # Create a subplot with 1 row and 2 columns
+    fig, ax1  = plt.subplots(1, 1)
+    fig.set_size_inches(10, 5)
+
+    ax1.set_xlim([-1, 1])
+    # The (n_clusters+1)*10 is for inserting blank space between silhouette
+    # plots of individual clusters, to demarcate them clearly.
+    ax1.set_ylim([0, len(silhouette_data)*1.05])
+
+    y_lower = 0
+    for i in range(1,n_clusters+1):
+    # Aggregate the silhouette scores for samples belonging to
+        # cluster i, and sort them
+        ith_silhouette_values = silhouette_values[cluster_labels == i]
+
+        ith_silhouette_values.sort()
+
+        size_cluster_i = ith_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                          0, ith_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.9, y_lower + 0.5 * size_cluster_i,
+                 'Cluster {}'.format(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 0  # 10 for the 0 samples
+
+    #    ax1.set_title("The silhouette plot for the various clusters.")
+        ax1.set_xlabel("The silhouette coefficient values")
+        ax1.set_ylabel("Cluster label")
+
+        # The vertical line for average silhouette score of all the values
+        ax1.axvline(x=silhouette_average, color="red", linestyle="--")
+
+        #ax1.set_yticks([])  # Clear the yaxis labels / ticks
+
+        ax1.set_xticks(np.arange(-1,1.2,0.2))
+
+
+    plt.suptitle("Silhouette for method {} and metric {} , with {} clusters".format(method,distance, n_clusters),fontsize=14, fontweight='bold')
+
+    #plt.tight_layout()
+
+    plt.savefig('../images/hierarchical/silhouette_{}_{}_n{}.pdf'.format(method,distance,n_clusters))
+
+    return silhouette_average
 
 plt.close()
 
 
+for n_clusters in range(2,5):
+    print(n_clusters)
+    calc_silhouette(n_clusters)
 
+
+calc_silhouette(2)
 
 
 
 ###########################################################
+###########################################################
+###########################################################
 import sklearn.metrics
 
+n_clusters = 2
+
+cluster_labels = fcluster(data_link, (n_clusters), criterion="maxclust")
+
 jaccard = sklearn.metrics.jaccard_similarity_score
+
+df.Left.hist()
+
+plt.hist(cluster_labels)
+
+jaccard()
+
+
+plt.close()
 
 jaccard(ward_labels,average_labels)
 jaccard(ward_labels_inv,average_labels)
@@ -203,46 +274,5 @@ jaccard(ward_labels_inv,df.Left)
 
 jaccard(average_labels,df.Left)
 
-# silhouette analysis
-
-labels = fcluster(data_link, 3, criterion="maxclust")
-silhouette_coefficient = silhouette_score(squareform(data_dist),labels,
-                                          metric='euclidean')
 
 
-plt.close()
-plt.hist(labels)
-plt.hist(labels2)
-
-#prova_cuttre = silhouette_coefficient
-
-
-silhouette_labels = silhouette_samples(squareform(data_dist),labels)
-
-df_silhouette = pd.DataFrame(np.array([labels,silhouette_labels]).T)
-df_silhouette.columns = ['cluster','silhouette']
-df_silhouette=df_silhouette.sort_values(['cluster','silhouette'],ascending=False)
-
-indici = np.arange(df_silhouette.shape[0],0,step=-1)
-df_silhouette = df_silhouette.assign(indici=indici)
-
-
-fig = plt.figure(figsize=(10,5))
-
-#plt.plot(x = df_silhouette['silhouette'],y=df_silhouette['indici'])
-plt.scatter(x=df_silhouette['silhouette'],y=df_silhouette['indici'],c=df_silhouette['cluster'])
-
-plt.title('Silhouette, Method: {} , Metric: {}, Coph Corr= {:.2f}, Silh. Coeff={:.2f} '.format(method,distance,coph_corr,silhouette_coefficient))
-
-plt.axvline(x=silhouette_coefficient)
-
-plt.savefig('../images/hierarchical/silhouette_{}_{}.pdf'.format(method,distance))
-
-
-plt.close()
-
-del(coph)
-del(data_link)
-
-
-gc.collect()
