405e9050e17d46e7fc6533eedd36ed96dc3be94c
Stefano Carpita <carpitastefano@gmail.com>
2017-12-01 20:26:19 +0100

    updated

--- code/hierarchical.py
+++ code/hierarchical.py
@@ -13,12 +13,15 @@ MEDIUM_SIZE = 20
 BIGGER_SIZE = 20
 
 plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
 plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y label
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc('titlesize', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
 
 ###########################################################
 # variables transformation
@@ -27,8 +30,9 @@ df = pd.read_csv('../data/df_formatted.csv')
 
 df.dtypes
 
-df_num = df.drop(['Left','Sales','Salary','Work_Accident','Promotion_Last_5_Years'],axis=1)
+df_num = df.drop(['Left','Department','Salary','Work_Accident','Promotion_Last_5_Years'],axis=1)
 
+df_num.dtypes
 df_num.shape
 
 from sklearn import preprocessing
@@ -51,7 +55,11 @@ data = df_norm
 # clustering with scipy
 from scipy.spatial.distance import pdist, squareform
 
-from scipy.cluster.hierarchy import linkage, dendrogram, cophenet, fcluster, cut_tree
+
+from scipy.cluster.hierarchy import linkage, dendrogram, cophenet, fcluster, cut_tree, inconsistent
+
+import scipy.cluster.hierarchy as h
+
 from sklearn.metrics import silhouette_score, silhouette_samples
 
 import gc
@@ -59,6 +67,8 @@ import gc
 
 methods = ['single','complete','average','weighted','centroid','median','ward']
 distances = ['euclidean','cityblock','mahalanobis']
+
+
 
 # testing function for analysis
 ###########################################################
@@ -71,10 +81,16 @@ data_link = linkage(data_dist,method=method,metric=distance)
 coph = cophenet(Z = data_link, Y = data_dist)
 coph_corr=coph[0]
 
+inconsistency = inconsistent(data_link,d=2)
+
+prova = h.maxinconsts(data_link,inconsistency)
+
+plt.hist(prova)
+
 plt.close()
 
 fig = plt.figure(figsize=(10, 5))
-dn = dendrogram(Z=data_link,p=3,truncate_mode='level',
+dn = dendrogram(Z=data_link,p=7,truncate_mode='level',
                 orientation='left',count_sort='ascendent',
                 no_labels=False,show_leaf_counts=True,
 )
@@ -136,19 +152,53 @@ Dout.columns = ['method','distance','coph_corr']
 
 Dout = Dout.sort_values(by=['coph_corr','method','distance'],ascending=False)
 
+
+
 import seaborn as sb
 plt.close()
 
-sb.set_style('whitegrid')
-plt.figure(figsize=(12,5))
-sb.barplot(data=Dout, x='method',y='coph_corr',
-           hue='distance')
-plt.title('Comparison of different methods')
-plt.tight_layout()
 
-plt.savefig('../images/hierarchical/methods_comparison.pdf')
+current_palette = sns.color_palette('deep')
+sns.palplot(current_palette)
+
+colors = ["windows blue", "amber", "faded green", "dusty purple"]
+sns.palplot(sns.xkcd_palette(colors))
+
+mypalette = sns.color_palette('husl',3)
+sns.palplot(mypalette)
+
+sns.set_palette(palette = 'huls',3)
+
+sb.xkcd_rgb.keys()
+
+colors = ["olive yellow","windows blue", "orange red"]
+
+palette = sns.xkcd_palette(colors)
+sb.palplot(palette)
+sb.set_palette(palette)
+
+sb.set_style('whitegrid')
 
 plt.close()
+plt.figure(figsize=(16,5))
+
+sb.barplot(data=Dout, x='method',y='coph_corr',
+           hue='distance')
+
+plt.legend(bbox_to_anchor=(1, 1), borderaxespad=0.)
+plt.tight_layout()
+plt.xlabel('Method')
+plt.ylabel('Cophenetic correlation')
+plt.title('Comparison of different methods')
+plt.tight_layout(pad=1)
+
+plt.savefig('../images/hierarchical/methods_comparison.pdf')
+plt.close()
+
+
+from ggplot import *
+ggplot(aes(weight='coph_corr',x='method'),data=Dout)+geom_bar(stat='identity')
+
 
 ###########################################################
 # from the selected method which are the best?
