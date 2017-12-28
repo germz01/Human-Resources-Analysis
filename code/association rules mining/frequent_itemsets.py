2ec35c1f3e3e905f10263bb8c12c38ca30c72f73
Stefano Carpita <carpitastefano@gmail.com>
2017-12-28 16:08:12 +0100

    initial commit

--- code/association rules mining/frequent_itemsets.py
+++ code/association rules mining/frequent_itemsets.py
@@ -3,6 +3,8 @@ import math
 import pandas as pd
 from fim import apriori
 
+n=14999
+k = math.ceil(math.log(n, 2)) + 1
 
 def sturges(n=14999):
     k = math.ceil(math.log(n, 2)) + 1
@@ -21,6 +23,7 @@ DS['Department'].replace(['sales', 'technical', 'support', 'IT',
                           'product_mng', 'marketing', 'RandD', 'accounting',
                           'hr', 'management'], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
                          inplace=True)
+
 DS['SL_100'] = DS['Satisfaction_Level']*100
 DS['LE_100'] = DS['Last_Evaluation']*100
 DS['AMHGroup'] = pd.cut(DS['Average_Montly_Hours'], bins=sturges(),
@@ -48,7 +51,10 @@ DS.drop(['Satisfaction_Level', 'Last_Evaluation', 'Average_Montly_Hours',
 print 'APPLYING APRIORI ALGORITHM'
 
 records = DS.to_records(index=False)
-supp = int(raw_input('SUPPORT: '))
+
+#supp = int(raw_input('SUPPORT: '))
+supp=2
+
 
 for target in ['s', 'c', 'm']:
     itemsets = apriori(records, supp=supp, zmin=2, target=target, report='s')
@@ -67,3 +73,53 @@ for target in ['s', 'c', 'm']:
         for record in itemsets:
             support = str(round(float(record[1]), 2))
             csv_writer.writerow({'ITEMSET': record[0], 'SUPPORT': support})
+
+###########################################################
+
+# inizio analisi Ste: estrazione delle regole
+
+report_dict = {'l':'support','c':'confidence','l':'lift'}
+report = "".join(report_dict.keys())
+#report_list = report.split()
+
+supp = 5
+conf= 80
+
+for target in ['r']:
+    out = apriori(records, supp=supp, conf=conf,
+                  zmin=2,target=target, report=report)
+
+
+len(out)
+    
+
+###########################################################
+## scrittura su file        
+fieldnames = ['Consequent', 'Antecedent',
+              'Supp','Conf','Lift']
+
+fname = '../../data/rules_supp{}_conf{}.csv'.format(supp,conf)
+with open(fname, 'wb') as f:
+    csv_writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=',')
+    csv_writer.writeheader()
+
+    for record in out:
+        ## costruisco il dizionario per writerow
+        csv_dict = {fieldnames[0] : record[0]}
+        for i,field in enumerate(fieldnames):
+            if type(record[i]) is float:
+                csv_dict.update({field :round(record[i],2)})
+            else:
+                csv_dict.update({field : record[i]})
+
+        csv_writer.writerow(csv_dict)
+
+###########################################################    
+#df_items = pd.read_csv("../../data/frequent_itemsets_20_c.csv")
+
+
+df_rules = pd.read_csv("../../data/rules_supp10_conf80.csv")
+df_rules = df_rules.sort_values(by=['Lift'],ascending=False)
+
+
+df_rules.shape
