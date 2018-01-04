5a6f7b2db5fae83af5f6ca7df1140d3b9b7c6610
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2018-01-04 17:45:47 +0100

    Added confusion matrices

--- code/rules/classifier.py
+++ code/rules/classifier.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from ast import literal_eval
 from sklearn.metrics import confusion_matrix
+from sklearn import metrics
 
 sns.set()
 sns.set_style("whitegrid")
@@ -12,8 +13,8 @@ ds = pd.read_csv('../../data/rules/dataset_rules.csv')
 ds.drop('Unnamed: 0', axis=1, inplace=True)
 
 
-def classify(record):
-    rules = pd.read_csv('~/Desktop/rules_left_class_c85.csv')
+def classify(record, csv):
+    rules = pd.read_csv('~/Desktop/rules_left_class_c' + csv + '.csv')
     ants = rules['Antecedent'].tolist()
 
     for rule in ants:
@@ -42,15 +43,20 @@ def classify(record):
 target = ds['Left'].map({'Y_L': 1, 'N_L': 0}).tolist()
 pred_target = list()
 
-for record in range(0, 14999):
-    row = ds.iloc[record].tolist()
-    pred_target.append(classify(row))
+for csv in ['85', '90', '95']:
+    print 'CLASSIFICO CON CSV ' + csv
+    for record in range(0, 14999):
+        row = ds.iloc[record].tolist()
+        pred_target.append(classify(row, csv))
 
-cm = confusion_matrix(target, pred_target)
+    cm = confusion_matrix(target, pred_target)
 
-sns.heatmap(cm, cmap='Spectral', cbar=True)
-plt.ylabel('True label')
-plt.xlabel('Predicted label')
+    sns.heatmap(cm, cmap='Spectral', cbar=True)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
-plt.savefig(fname='../../images/rules/confusion_matrix.pdf', format='pdf',
-            bbox_inches='tight')
+    plt.savefig(fname='../../images/rules/confusion_matrix' + csv + '.pdf',
+                format='pdf', bbox_inches='tight')
+    plt.clf()
+
+    pred_target = list()
